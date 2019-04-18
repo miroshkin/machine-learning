30ec2a81087a5e7084eb5e6ff709e1efcd770859
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-04-18 15:20:39 +0300

    Init commit

---
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_import_libs.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Редактор Spyder
+print ("Hello, my kitty!")
+Это временный скриптовый файл.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
